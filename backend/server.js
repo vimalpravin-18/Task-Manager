1aa2b82124b1784916eb181e1dd0946097e72037
vimalpravin-18 <vimalsuruthika071@gmail.com>
2026-06-13 12:40:52 +0530

    Domain authorization

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "https://task-manager-blond-seven-67.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Initialize PostgreSQL database and start server
